f47ccca6d09f0d1bf10c48d2b48079d88533afd8
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-06-24 10:08:37 +0900

    feat: alert message

--- sample/app/src/app/devices/nfc/nfc.service.ts
+++ sample/app/src/app/devices/nfc/nfc.service.ts
@@ -12,12 +12,18 @@ export async function webnfc() {
     // w3c-web-nfcを使用するのが本式
     ndef.addEventListener('reading', ({ message, serialNumber }) => {
       console.log(`> Serial Number: ${serialNumber}`);
+      window.alert(`> Serial Number: ${serialNumber}`);
       console.log(`> Records: (${message.records.length})`);
+      window.alert(`> Records: (${message.records.length})`);
       for (const record of message.records) {
         console.log(`> Record type: ${record.recordType}`);
+        window.alert(`> Record type: ${record.recordType}`);
         console.log(`> MIME type: ${record.mediaType}`);
+        window.alert(`> MIME type: ${record.mediaType}`);
         console.log(`> Record id: ${record.id}`);
+        window.alert(`> Record id: ${record.id}`);
         console.log(`> Record data: ${record.data}`);
+        window.alert(`> Record data: ${record.data}`);
       }
     });
   } catch (error) {
