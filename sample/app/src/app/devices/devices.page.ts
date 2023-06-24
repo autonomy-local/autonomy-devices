589404ceefe9cde5f91c5f656749ec1cec7cf3b7
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-06-24 09:07:42 +0900

    chore: remove not use imports

--- sample/app/src/app/devices/devices.page.ts
+++ sample/app/src/app/devices/devices.page.ts
@@ -1,5 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'devices',
