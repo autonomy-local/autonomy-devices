fb053004d725314c9ab02ba6679edb782b74e6ba
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-06-24 12:02:58 +0900

    feat: add card reading

--- sample/app/src/app/devices/usb/usb.service.ts
+++ sample/app/src/app/devices/usb/usb.service.ts
@@ -5,6 +5,22 @@ const DEVICE_FILTER = {};
 const DEVICE_OPTIONS = {
   filters: [DEVICE_FILTER],
 };
+const ACK_PACKET = Uint8Array.of(0x00, 0x00, 0xff, 0x00, 0xff, 0x00);
+const command = Uint8Array.of(
+  0x00,
+  0x00,
+  0xff,
+  0xff,
+  0xff,
+  0x03,
+  0x00,
+  0xfd,
+  0xd6,
+  0x2a,
+  0x01,
+  0xff,
+  0x00
+);
 export async function webusb() {
   console.log('webusb()');
   try {
@@ -12,11 +28,27 @@ export async function webusb() {
     // w3c-web-usbを使用するのが本式
     const device = await navigator?.usb.requestDevice(DEVICE_OPTIONS);
     console.log('device', device);
+    window.alert('device: ' + device.productName);
     await device.open();
-    console.log('opened', device);
+    console.log('opened', device.productName);
+    window.alert('opened: ' + device.productName);
     await device.selectConfiguration(1);
-    console.log('configured', device);
+    console.log('configured', device.productName);
+    window.alert('configured: ' + device.productName);
+    await device.claimInterface(1);
+    console.log('claimed', device.productName);
+    window.alert('claimed: ' + device.productName);
+    const result = await device.transferOut(2, ACK_PACKET);
+    console.log('transferOut', result);
+    window.alert('transferOut: ' + JSON.stringify(result));
+    const result1 = await device.transferOut(2, command);
+    console.log('transferOut', result1);
+    window.alert('transferOut: ' + JSON.stringify(result1));
+    const result2 = await device.transferIn(2, 6);
+    console.log('transferIn', result2);
+    window.alert('transferIn: ' + JSON.stringify(result2));
   } catch (error) {
     console.error(error);
+    window.alert('error: ' + error);
   }
 }
